cca4a2c9ffa801f0f2f42e6b00bdfe0725b53920
Sudarshan Bhat <sudarsh.bhat@gmail.com>
2019-02-24 20:15:24 +0530

    Updates react-native-slang to use v1.0.1 of the Slang SDK.

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 
-import { NativeModules } from 'react-native';
+import { NativeModules, DeviceEventEmitter } from 'react-native';
 
-const { RNSlang } = NativeModules;
+const { SlangBuddy } = NativeModules;
 
-export default RNSlang;
+// RN wrapper for SlangBuddy. All the SlangBuddy integrations exposed over the native bridge are available here. 
+class Slang {
+
+  // Initializes Slang
+  static initialize(buddyId, apiKey, config, callback) {
+    SlangBuddy.initialize(buddyId, apiKey, config, callback);
+  }
+
+  // Listen to SlangIntentAction
+  static setIntentActionListener(actionListener) {
+    DeviceEventEmitter.addListener('slang_action', function(event) {
+      actionListener(event);
+    });
+  }
+
+  // Notify Slang of successful action
+  static notifyActionCompleted(success) {
+    SlangBuddy.notifyActionCompleted(success);
+  }
+}
+
+export default Slang;
